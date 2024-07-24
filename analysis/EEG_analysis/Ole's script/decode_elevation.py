ba0d1dbb08cfc533ad5e639354adaf0c0469710c
ellisnwj <152647376+ellisnwj@users.noreply.github.com>
2024-07-24 18:01:43 +0200

    Update decode_elevation.py

--- analysis/EEG_analysis/Ole's script/decode_elevation.py
+++ analysis/EEG_analysis/Ole's script/decode_elevation.py
@@ -7,24 +7,31 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from mne import read_epochs, concatenate_epochs
 from mne.decoding import SlidingEstimator, cross_val_multiscore
+import pickle
 
 eeg_dir = Path.cwd() / 'data' / 'experiment' / 'EEG'
-
+results_dir = Path.cwd() / 'data' / 'experiment' / 'results' / 'decoding'
+p = 100
+start_idx = 166
+stop_idx = 239
 # p = json.load(open(root / "code" / "parameters.json"))
 for sub in eeg_dir.glob("P?*"):
     print(sub)
+    results = {}
     for i_ses, ses in enumerate(sub.glob("ses*")):
         print(ses)
         epochs = [read_epochs(e) for e in ses.glob("*-epo.fif")][0]
 
         events, event_id = epochs.events, epochs.event_id
         n_times = len(epochs.times)
+
         combinations = [
             ",".join(map(str, c))
             for c in itertools.combinations(epochs.event_id.keys(), 2)
         ]
         combinations = [comb.split(",") for comb in combinations]
-        results = {}
+
+        condition_scores = []
         for i, (con1, con2) in enumerate(combinations):
             mask = np.logical_or(
                 events[:, 2] == event_id[con1], events[:, 2] == event_id[con2]
@@ -35,12 +42,19 @@ for sub in eeg_dir.glob("P?*"):
                 StandardScaler(), LogisticRegression(solver="lbfgs", max_iter=1000)
             )
             decoder = SlidingEstimator(clf, scoring="roc_auc", verbose=True, n_jobs=4)
-            scores = cross_val_multiscore(decoder, X, y, cv=p["cv_folds"])      #create a parameters list
-            results[f"{con1} vs {con2}"] = scores.mean(axis=0)
-            np.save(
-                eeg_dir
-                / "results"
-                / "decoding"
-                / f"{sub.name}_{ses.name}_decoding_score.npy",
-                results,
-            )
+            scores = cross_val_multiscore(decoder, X, y, cv=p)      #create a parameters list
+
+            condition_score = scores.mean(axis=0)  # mean across 100 folds of cross validation
+            condition_scores.append(condition_score)
+        # subject_score = subject_scores.mean(axis=1) # mean across conditions
+        subject_score = np.mean(condition_scores, axis=0)
+        subject_score = subject_score[start_idx:stop_idx].mean() # mean across time
+
+        results[f"{ses.name}"] = subject_score
+    # save dict as pickle
+    # create a binary pickle file
+    with open(results_dir / f"{sub.name}_scores.pkl","wb") as f:
+        # write the python object (dict) to pickle file
+        pickle.dump(results,f)
+        # close file
+    f.close()
